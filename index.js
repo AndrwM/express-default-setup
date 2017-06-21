5e089febae0efdac04ab1b0e345439dcd7301526
Amy Burka <aburka@gmail.com>
2017-06-21 13:44:52 -0400

    use mustache as view engine

--- index.js
+++ index.js
@@ -1,8 +1,13 @@
 const express = require('express');
 const application = express();
+const mustache = require('mustache-express');
+
+application.engine('mustache', mustache());
+application.set('views', './views');
+application.set('view engine', 'mustache');
 
 application.get('/', (request, response) => {
-  
+
 });
 
 application.listen(3000, () => {
